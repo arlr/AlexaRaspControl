9d2931fb6da0d7900629cff8cecb7059b18c19a8
arlr <arlrcode@gmail.com>
2018-12-23 22:12:09 +0100

    Ajout Recherche bleutooth

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AlexaRaspBluetooth
 Utiliser Amazon Alexa pour intéragir avec des appareil connecté en bleutooth à une Raspberry PI
+
+nécessite d'installer python-bluez "sudo apt-get install bluez python-bluez"
--- RaspCode/main.py
+++ RaspCode/main.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Source : https://gist.github.com/keithweaver/3d5dbf38074cee4250c7d9807510c7c3
+
+import bluetooth
+
+#Cherches appareils bluetooth
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("found %d devices" % len(nearby_devices))
+
+for addr, name in nearby_devices:
+    print("  %s - %s" % (addr, name))
