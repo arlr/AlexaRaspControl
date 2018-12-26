f3f2cedad68b62bc4381c65376376e828c859b59
arlr <arlrcode@gmail.com>
2018-12-26 17:03:31 +0100

    PRemiere vertion du code Arduino

--- µCcode/BluetoothRelay/src/main.cpp
+++ µCcode/BluetoothRelay/src/main.cpp
@@ -1,9 +1,47 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
+// Constantes
+#define RELAY 12
+#define TX 8
+#define RX 7
+
+SoftwareSerial hc05(RX , TX);
+
+void InitBL(){
+  pinMode(TX, OUTPUT);
+  pinMode(RX, INPUT);
+  hc05.begin(9600);
+  Serial.begin(9600);
+  SetupBL();
+}
+
+void SetupBL(){
+  hc05.write("AT+UART=9600,0,0"); //Speed
+  hc05.write("AT+NAME=LumBur"); //Name
+  hc05.write("AT+PSWD=1234"); //PassWord
+}
+
+readBL(){
+  char val;
+  if(hc05.available){
+    val = hc05.read()
+    if(val == '0'){
+      digitalWrite(RELAY,LOW);
+    }
+    if(val == '1'){
+      digitalWrite(RELAY,HIGH);
+    }
+  }
+}
 void setup() {
-  // put your setup code here, to run once:
+  
+  InitBL();
+  pinMode(RELAY,OUTPUT);
+  digitalWrite(RELAY,HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  readBL();
 }
