0c734e22bd4e04ac01acf9e19e241b83614adb50
arlr <arlrcode@gmail.com>
2018-12-29 22:03:18 +0100

    tests

--- RaspCode/Bl_Scripts/ListBL.py
+++ RaspCode/Bl_Scripts/ListBL.py
@@ -10,8 +10,8 @@ device = []
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
 #print("found %d devices" % len(nearby_devices))
 
-#for addr, name in nearby_devices:
- #   device.append(name)
+for addr, name in nearby_devices:
+    device.append(name)
   #  print("  %s - %s" % (addr, name))
     
 
