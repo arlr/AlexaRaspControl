142f05a10181b7eebde2ae641ac69b823ad641e1
arlr <arlrcode@gmail.com>
2018-12-30 16:46:57 +0100

    test

--- test/test.py
+++ test/test.py
@@ -2,4 +2,4 @@
 #https://www.developpez.net/forums/d956151/autres-langages/python-zope/general-python/recuperer-resultat-commandes-shell/
 
 import sys ,os,subprocess
-subprocess.Popen('python3 ../RaspCode/Bl_Scripts/ScanBL.py')
+subprocess.Popen(['python3', '../RaspCode/Bl_Scripts/ScanBL.py'])
