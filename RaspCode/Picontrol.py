008892cd4a3a5ed97dd3985963d90a0402a1e6ae
arlr <arlrcode@gmail.com>
2018-12-30 12:40:03 +0100

    arlr

--- RaspCode/Picontrol.py
+++ RaspCode/Picontrol.py
@@ -28,8 +28,9 @@ def scan_bluetooth():
 
 
     #Travaillé sur le problème du temp de réponse !!
-    devices = subprocess.run(["python3", "../RaspCode/Bl_Scripts/ListBL.py"],stdout=subprocess.PIPE)
-    listeDevice=devices.stdout.decode('utf-8')
+    #devices = subprocess.run(["python3", "../RaspCode/Bl_Scripts/ListBL.py"],stdout=subprocess.PIPE)
+    #listeDevice=devices.stdout.decode('utf-8')
+    str(listeDevice) = 2+2
     return statement('Les appareils disponibles sont :', listeDevice)
 
 @ask.session_ended
